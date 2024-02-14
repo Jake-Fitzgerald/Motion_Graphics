3000ccfb0680d6a01c58c6373ce4aa574bb41ddf
(Student  C00288105) Jake Fitzgerald <C00288105@setu.ie>
2024-02-14 16:45:47 +0000

    Started adding in code from word doc

--- Lab_3/Lab_3/Game.cpp
+++ Lab_3/Lab_3/Game.cpp
@@ -1,45 +1,24 @@
-/// <summary>
-/// @author Peter Lowe
-/// @date May 2019
-///
-/// you need to change the above lines or lose marks
-/// </summary>
+// Student Name: Jake Fitzgerald
+// Student Number: C00288105
 
 #include "Game.h"
 #include <iostream>
 
-
-
-/// <summary>
-/// default constructor
-/// setup the window properties
-/// load and setup the text 
-/// load and setup thne image
-/// </summary>
 Game::Game() :
-	m_window{ sf::VideoMode{ 800U, 600U, 32U }, "SFML Game" },
+	m_window{ sf::VideoMode{ 800U, 600U, 32U }, "One Button Platformer - Jake Fitzgerald C00288105" },
 	m_exitGame{false} //when true game will exit
 {
-	setupFontAndText(); // load font 
-	setupSprite(); // load texture
+
+
 }
 
-/// <summary>
-/// default destructor we didn't dynamically allocate anything
-/// so we don't need to free it, but mthod needs to be here
-/// </summary>
+
 Game::~Game()
 {
 }
 
 
-/// <summary>
-/// main game loop
-/// update 60 times per second,
-/// process update as often as possible and at least 60 times per second
-/// draw as often as possible but only updates are on time
-/// if updates run slow then don't render frames
-/// </summary>
+
 void Game::run()
 {	
 	sf::Clock clock;
@@ -59,11 +38,7 @@ void Game::run()
 		render(); // as many as possible
 	}
 }
-/// <summary>
-/// handle user and system events/ input
-/// get key presses/ mouse moves etc. from OS
-/// and user :: Don't do game update here
-/// </summary>
+
 void Game::processEvents()
 {
 	sf::Event newEvent;
@@ -81,10 +56,6 @@ void Game::processEvents()
 }
 
 
-/// <summary>
-/// deal with key presses from the user
-/// </summary>
-/// <param name="t_event">key press event</param>
 void Game::processKeys(sf::Event t_event)
 {
 	if (sf::Keyboard::Escape == t_event.key.code)
@@ -93,10 +64,7 @@ void Game::processKeys(sf::Event t_event)
 	}
 }
 
-/// <summary>
-/// Update the game world
-/// </summary>
-/// <param name="t_deltaTime">time interval per frame</param>
+
 void Game::update(sf::Time t_deltaTime)
 {
 	if (m_exitGame)
@@ -111,41 +79,57 @@ void Game::update(sf::Time t_deltaTime)
 void Game::render()
 {
 	m_window.clear(sf::Color::White);
-	m_window.draw(m_welcomeMessage);
-	m_window.draw(m_logoSprite);
+
 	m_window.display();
 }
 
-/// <summary>
-/// load the font and setup the text message for screen
-/// </summary>
-void Game::setupFontAndText()
+
+
+void Game::init()
 {
+
+	// Font Setup
 	if (!m_ArialBlackfont.loadFromFile("ASSETS\\FONTS\\ariblk.ttf"))
 	{
 		std::cout << "problem loading arial black font" << std::endl;
 	}
-	m_welcomeMessage.setFont(m_ArialBlackfont);
-	m_welcomeMessage.setString("SFML Game");
-	m_welcomeMessage.setStyle(sf::Text::Underlined | sf::Text::Italic | sf::Text::Bold);
-	m_welcomeMessage.setPosition(40.0f, 40.0f);
-	m_welcomeMessage.setCharacterSize(80U);
-	m_welcomeMessage.setOutlineColor(sf::Color::Red);
-	m_welcomeMessage.setFillColor(sf::Color::Black);
-	m_welcomeMessage.setOutlineThickness(3.0f);
 
-}
+	m_view = m_window.getDefaultView();
+	// Player Setup
+	m_playerShape.setSize(sf::Vector2f(20, 20));
+	m_playerShape.setPosition(160, 500);
 
-/// <summary>
-/// load the texture and setup the sprite for the logo
-/// </summary>
-void Game::setupSprite()
-{
-	if (!m_logoTexture.loadFromFile("ASSETS\\IMAGES\\SFML-LOGO.png"))
+	for (int row = 0; row < HORIZONTAL_NUM; row++)
 	{
-		// simple error message if previous call fails
-		std::cout << "problem loading logo" << std::endl;
+		for (int col = 0; col < VERTICAL_NUM; col++)
+		{
+
+			if (levelData[row][col] == 1)
+			{
+
+				m_level[row][col].setSize(sf::Vector2f(70, 30));
+				m_level[row][col].setPosition(row * 70, col * 30);
+				m_level[row][col].setFillColor(sf::Color::Red);
+			}
+			if (levelData[row][col] == 0)
+			{
+
+				m_level[row][col].setSize(sf::Vector2f(70, 30));
+				m_level[row][col].setPosition(row * 70, col * 30);
+				m_level[row][col].setFillColor(sf::Color::Black);
+			}
+			if (levelData[row][col] == 2)
+			{
+				m_level[row][col].setSize(sf::Vector2f(70, 30));
+				m_level[row][col].setPosition(row * 70, col * 30);
+
+				m_level[row][col].setFillColor(sf::Color::Blue);
+
+			}
+
+		}
+		std::cout << std::endl;
+
 	}
-	m_logoSprite.setTexture(m_logoTexture);
-	m_logoSprite.setPosition(300.0f, 180.0f);
 }
+
